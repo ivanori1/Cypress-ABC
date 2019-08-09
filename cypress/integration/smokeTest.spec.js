d03eb8313f1b2a78c507abe0cab1a78601c0700f
ivanori1 <coric.ivan@yandex.com>
2019-08-09 16:29:08 +0200

    init commit

---
+++ cypress/integration/smokeTest.spec.js
@@ -0,0 +1,14 @@
+describe('smokeTest', () => {
+  before(() => {
+    cy.visit('/')
+  })
+  context('Design Page', () => {
+    const Design = "Framsidan"
+    it('Assert Configurator with navigation, viewpoint, 3D hat and container-nav is loaded', () => {
+      cy.assertConfiguratorLoaded();
+    })
+    it('Assert Length of nav buttons is 5 and DesignForslag is selected', () => {
+      cy.assertDesignForslagSelected();
+    })
+  })
+})
