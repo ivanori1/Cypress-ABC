9d4c10047ffc9884137320a4ad5b035760f6e0a4
ivanori1 <coric.ivan@yandex.com>
2019-08-11 20:43:34 +0200

    fixture implement

--- cypress/integration/smokeTest.spec.js
+++ cypress/integration/smokeTest.spec.js
@@ -1,26 +1,22 @@
 /**
- * Main spec called smokeTest that contains various test cases
+ * Main spec called Smoke Test that contains various test cases
+ * inside of context (Test case groups)
  * cy. calls commands from support/comands.js 
  */
 
-describe('smokeTest', () => {
-  // Navigation buttons
-  const navFramsidan = "Framsidan"
-  const navBaksidan = "Baksidan"
-  const navInsidan = "Insidan"
-  const navOvansidan = "Ovansidan"
-  const email = "123@abc.gruppen"
-  const leftInput = "Srbija"
-  const rightInput = "Do Tokija"
+describe('Smoke Test', () => {
+
   before(() => {
-    // it is only '/' caled to visit because default url is set in cypres.json
+    // it is only call visti '/' because default url is set in cypres.json
     cy.visit('/')
   })
+
   context('Design Page', () => {
 
 
     it('Assert Configurator with navigation, viewpoint, 3D hat and container-nav is loaded', () => {
       cy.assertConfiguratorLoaded();
+
     })
     it('Assert Length of nav buttons is 5 and DesignForslag is selected', () => {
       cy.assertDesignForslagSelected();
@@ -29,29 +25,36 @@ describe('smokeTest', () => {
       cy.checkNavBarMain();
     })
     it('Click on selected navigation button', () => {
-      cy.assertNavSelected(navInsidan)
-      cy.assertNavSelected(navFramsidan)
-      cy.assertNavSelected(navBaksidan)
-      cy.assertNavSelected(navOvansidan)
+      cy.fixture('buttons').as('buttonsFixture')
+      cy.get('@buttonsFixture').then(button => {
+        cy.assertNavSelected(button.navigation[1])
+        cy.assertNavSelected(button.navigation[2])
+        cy.assertNavSelected(button.navigation[3])
+        cy.assertNavSelected(button.navigation[4])
+      })
+
     })
     it('Save Design ID to mail', () => {
       cy.clickSaveDesignBtn();
       cy.saveDesignNegative();
-      cy.saveDesignPositive(email);
+      cy.saveDesignPositive();
     })
-  })
+    it('Design a Hat', () => {
+      cy.designHat();
+    })
 
-  context('Checkout page', () => {
+  });
 
-    it('Design and Checkout', () => {
-      cy.designHat(leftInput, rightInput);
+  context('Checkout Page', () => {
+    it('Checkout', ()=> {
       cy.checkOut()
     })
+
     it('Select First Available Size of hat', () => {
       cy.selectSize();
-      cy.completeOrderNegative(email);
     })
-  })
-
-
+    it('Confirm the order', () => {
+        cy.completeOrderNegative();
+      })
+    })
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,31 @@
 /**
- * The support/commands.js is file where all separted commands are placed
+ * The support/commands.js is file where all separated commands are placed
  * Cypress.Commands.add() create new function that could be call and reuse
- * multiple time in any integration/spec.js 
+ * multiple time in any integration/spec.js file
  */
 
- 
 /*
 Command for verifiying every element visiblity in home-page
- method get take CSS and should assert it is visible
+ method get CSS (from fixture/locators.json) and should assert it is visible
 */
 
+
 Cypress.Commands.add('assertConfiguratorLoaded', () => {
-    cy.get('[data-name="configurator"]')
-        .should('be.visible')
-    cy.get('#hatConfigurator')
-        .should('be.visible')
-    cy.get('#hatConfigurator [class="nav nav-tabs nav-tabs-theme"]')
-        .should('be.visible')
-    cy.get('.viewport')
-        .should('be.visible')
-    cy.get('[class="nav-panel mb-3"]')
-        .should('be.visible')
-    cy.get('.hat3D')
-        .should('be.visible')
+    cy.fixture('locators').as('locatorFixture')
+    cy.get('@locatorFixture').then(locator => {
+        cy.get(locator.wholePageConfigurator)
+            .should('be.visible')
+        cy.get(locator.hatConfigurator)
+            .should('be.visible')
+        cy.get(locator.hatConfiguratorNavButtonsList)
+            .should('be.visible')
+        cy.get(locator.hatConfiguratorViewPort)
+            .should('be.visible')
+        cy.get(locator.footerPanelNavigation)
+            .should('be.visible')
+        cy.get(locator.hat3Danimation)
+            .should('be.visible')
+    })
 
 })
 /*
@@ -31,15 +34,20 @@ is there 5 navigation buttons, also check is default Design selected
 */
 
 Cypress.Commands.add('assertDesignForslagSelected', () => {
-    cy.get('#hatConfigurator > .nav > li').
-        should('have.length', 5)
-    cy.get('#hatConfigurator > ul > li:first')
-        .should('have.class', 'active')
-        .should('contain', 'Designförslag')
-    // Please tell developers not to call this data-name undefined 
-    cy.get('[data-name="undefined"]')
-    .should('be.visible')
+    cy.fixture('locators').as('locatorFixture')
+    cy.get('@locatorFixture').then(locator => {
 
+        cy.get(locator.hatConfiguratorNavButtonsEach).
+            should('have.length', 5)
+        cy.get(locator.hatConfiguratorNavButtonsEach)
+            .first()
+            .should('have.class', 'active')
+            .should('contain', 'Designförslag')
+        // Please tell developers not to call this data-name undefined 
+        cy.get('[data-name="undefined"]')
+            .should('be.visible')
+
+    })
 })
 
 /*
@@ -47,11 +55,18 @@ Command take variable navigation that is saved as constant
 in spec.js file click on that and asert drop-menu is visible
 */
 Cypress.Commands.add('assertNavSelected', (navigation) => {
-    cy.get('#hatConfigurator > ul > li > div')
-        .contains(navigation)
-        .click()
-    cy.get('ul[class="nav nav-tabs nav-tabs-secondary nav-tabs-theme"]')
-        .should('be.visible')
+    cy.fixture('locators').as('locatorFixture')
+    cy.get('@locatorFixture').then(locator => {
+
+        cy.get(locator.hatConfiguratorNavButtonTitle)
+            .contains(navigation)
+            .click()
+        cy.get(locator.hubConfiguratorNavButtonDropDown)
+            .should('be.visible')
+        cy.get(locator.hatConfiguratorNavButtonTitle)
+            .contains(navigation)
+            .click()
+    })
 })
 /*
 Command check navigation bar menu (hamburger icon) and click on it
@@ -60,13 +75,17 @@ After clicking it assert thet list in hamburger menu have 3 items
 */
 Cypress.Commands.add('checkNavBarMain', () => {
     cy.get('nav').within(() => {
-        cy.get('button[aria-controls="navbarMain"]')
-            .click()
-            .should('not.have.class', 'collapsed')
-        cy.get('#navbarMain')
-            .should('have.class', 'show')
-        cy.get('#navbarMain li')
-            .should('have.length', 3)
+        cy.fixture('locators').as('locatorFixtures')
+        cy.get('@locatorFixtures').then(locator => {
+            cy.get(locator.hamburgerNavButton)
+                .click()
+                .should('not.have.class', 'collapsed')
+            cy.get(locator.hamburgerDropdown)
+                .should('have.class', 'show')
+            cy.get(locator.hamburgerDropdownList)
+                .should('have.length', 3)
+        })
+
     })
 })
 
@@ -75,13 +94,18 @@ His comand click on Save design button whitin navigation bar
 Then check is Save design pop-up visible
 */
 Cypress.Commands.add('clickSaveDesignBtn', () => {
-    cy.get('nav').within(() => {
+    cy.fixture('locators').as('locatorFixture')
+    cy.get('@locatorFixture').then(locator => {
+        cy.get('nav').within(() => {
 
-        cy.get('[title="Spara din design"]')
-        .click()
+            cy.get(locator.saveDesignNavButton)
+                .click()
+        })
+        cy.get(locator.popUp)
+            .should('be.visible')
+
     })
-    cy.get('.modal-content')
-    .should('be.visible')
+
 })
 /*
 This command test email input and error message in it
@@ -89,46 +113,67 @@ This command test email input and error message in it
 2 test case wrong email format
 */
 Cypress.Commands.add('saveDesignNegative', () => {
-    cy.get('.modal-content .btn-primary')
-        .click()
-    cy.get('.invalid-feedback').should('contain', 'Fältet E-post har inte ifyllts.')
-    cy.get('[name="email"]')
-        .type('1')
-        .should('have.class', 'is-invalid')
-    cy.get('.invalid-feedback').should('contain', 'E-post har fel format.')
+
+    cy.fixture('locators').as('fixtureLocator')
+    cy.get('@fixtureLocator').then(locator => {
+        cy.get(locator.popUpConfirmButton)
+            .click()
+        cy.get(locator.inputError)
+            .should('contain', 'Fältet E-post har inte ifyllts.')
+        cy.get('[name="email"]')
+            .should('have.class', 'is-invalid')
+            .type('1')
+            .should('have.class', 'is-invalid')
+        cy.get(locator.inputError).should('contain', 'E-post har fel format.')
+    })
 
 })
 
 /*
-Command gives email variable from spec.js file and click on save btn
+Command gives email variable from fixture/user.json file and click on save btn
 */
-Cypress.Commands.add('saveDesignPositive', (email) => {
-    cy.get('[name="email"]')
-    .type('{backspace}')
-        .type(email)
-    cy.get('.modal-content .btn-primary')
-        .click()
-    cy.get('.modal-content .btn-secondary')
-        .click()
+
+Cypress.Commands.add('saveDesignPositive', () => {
+    // Fixture is set in fixture/user.json : Tester can change data there
+    cy.fixture('user').as('userFixture')
+    cy.get('@userFixture').then(user => {
+
+        cy.get('[name="email"]')
+            .type('{backspace}')
+            .first()
+            .type(user.email)
+        cy.get('.modal-content .btn-primary')
+            .click()
+        cy.get('.modal-content .btn-secondary')
+            .click()
+    })
 })
 /*
 This command is creating simple hat
 clicking on [Create] button
 typing text on front of hat
 */
-Cypress.Commands.add('designHat', (left, right) => {
-    cy.get('[data-name="undefined"] .btn-primary')
-        .click()
-    cy.get('[data-name="frontEmbroidery"] h3')
-        .should('contain', 'Brodyr framsida')
-    cy.get('[class="form-control input-left"]')
-        .type('Srbija')
-        .should('have.value', left)
-    cy.get('[class="form-control input-right"]')
-        .type(right)
-        .should('have.value', right)
+Cypress.Commands.add('designHat', () => {
+    cy.fixture('design').as('designFixture')
+    cy.get('@designFixture').then(design => {
+
+        cy.get('[data-name="undefined"] .btn-primary')
+            .click()
+        cy.get('[data-name="frontEmbroidery"] h3')
+            .should('contain', 'Brodyr framsida')
+        cy.get('[class="form-control input-left"]')
+            .type('Srbija')
+            .should('have.value', design.front.embroidery.left)
+        cy.get('[class="form-control input-right"]')
+            .type(design.front.embroidery.right)
+            .should('have.value', design.front.embroidery.right)
+    })
 })
 
+/*
+This comand click on shoping card, in chlothes
+click again on checkout button and assert url conatins checkout
+*/
 Cypress.Commands.add('checkOut', () => {
     cy.get('.shopping-cart-popover-target')
         .click()
@@ -149,7 +194,10 @@ Cypress.Commands.add('checkOut', () => {
         .should('eq', 'https://studentmossa.abcgruppen.se/#checkout')
 })
 
-
+/*
+Select first available size
+and assert specific size button is active (have red border)
+*/
 Cypress.Commands.add('selectSize', () => {
     cy.get('.btn-primary')
         .click()
@@ -163,9 +211,18 @@ Cypress.Commands.add('selectSize', () => {
         .click()
 
 })
-Cypress.Commands.add('completeOrderNegative', (email) => {
+/*
+Click on Confirm order when requred data is empty
+Use fixture data to assert that previously used email
+is auto filled
+*/
+Cypress.Commands.add('completeOrderNegative', () => {
+    cy.fixture('user').as('userFixture')
     cy.get('.btn-primary')
         .click()
     cy.get('.is-invalid').should('have.length', 9)
-   cy.get('[type="email"]').should('have.value', email)
+    cy.get('@userFixture').then(user => {
+        cy.get('[type="email"]').should('have.value', user.email)
+    cy.get('[name="className"]').should('be.disabled')
+    })
 })
